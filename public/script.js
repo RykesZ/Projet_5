d2a62fa09c8c41aefc31280038b4fcc7546e3249
RykesZ <adfourretout@hotmail.fr>
2021-02-03 19:32:39 +0100

    panier commencé

--- public/script.js
+++ public/script.js
@@ -1,8 +1,20 @@
 const result = document.getElementById('result');
 const merchList = document.getElementById('merchList');
 const productPresentation = document.getElementById('productPresentation');
+const addCartButton = document.getElementById('addCart');
+var cartItems = [];
+sessionStorage.setItem('initialised', 'false');
 
 const api = 'http://localhost:3000/api/teddies';
+
+const cartInitialisation = () => {
+    if (sessionStorage.getItem('initialised') === 'false') {
+        sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+        sessionStorage.setItem('initialised', 'true');
+    }
+}
+
+cartInitialisation();
 
 function makeRequest(verb, url, data) {
     return new Promise((resolve, reject) => {
@@ -67,8 +79,9 @@ async function getProductList() {
             const productImage = document.createElement("div");
             productImage.classList.add("col-3");
             const imageContent = document.createElement("img");
-            imageContent.setAttribute("src", productList[i].imageUrl)
-            imageContent.setAttribute("alt", "Ours en peluche")
+            imageContent.setAttribute("src", productList[i].imageUrl);
+            imageContent.setAttribute("alt", "Ours en peluche");
+            imageContent.classList.add("thumbnail");
             productImage.appendChild(imageContent);
             newProduct.appendChild(productImage);
 
@@ -85,16 +98,50 @@ async function getProductDetails(id) {
         const requestPromise = makeRequest('GET', api + '/' + id);
         const productDetails = await requestPromise;
 
-        const newProduct = document.createElement("div");
-        newProduct.classList.add("row");
-        const verifId = document.createElement("div");
-        verifId.classList.add("col");
-        verifId.textContent = id;
+        const imageHolder = document.getElementById('productImage');
+        const nameHolder = document.getElementById('name');
+        const priceHolder = document.getElementById('price');
+        const descriptionHolder = document.getElementById('description');
+        const colorChoice = document.getElementById('color');
 
-        newProduct.appendChild(verifId);
-        productPresentation.appendChild(newProduct);
+        imageHolder.setAttribute('src', productDetails.imageUrl);
+        imageHolder.classList.add('productImage');
+        nameHolder.textContent = productDetails.name;
+        priceHolder.textContent = productDetails.price;
+        descriptionHolder.textContent = productDetails.description;
+
+        for (let i = 0; i < productDetails.colors.length; ++i) {
+            const newOption = document.createElement('option');
+            newOption.setAttribute('value', productDetails.colors[i]);
+            newOption.textContent = productDetails.colors[i];
+            colorChoice.appendChild(newOption);
+        }
 
     } catch (errorResponse) {
         //result.textContent = errorResponse.error;
     };
+};
+
+addCartButton.addEventListener('click', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    cartItems = JSON.parse(sessionStorage.getItem('cartItems'));
+    cartItems.push(sessionStorage.getItem('currentProductId'));
+    sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+    console.log(cartItems);
+    console.log(sessionStorage.getItem('cartItems'));
+    //window.open('./panier.html', '_self');
+});
+
+async function getCartDetails() {
+    try {
+        cartItems = JSON.parse(sessionStorage.getItem('cartItems'));
+        console.log(cartItems);
+        for (let i = 0; i < cartItems.length; ++i) {
+            const requestPromise = makeRequest('GET', api + '/' + cartItems[i]);
+            const cartItemDetails = await requestPromise;
+        }
+    } catch (errorResponse) {
+        
+    }
 };
